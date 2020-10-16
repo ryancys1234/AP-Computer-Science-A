cf874a3644eeb6a37c072eb15ebdf0765c754be1
ryancys1234 <55104016+ryancys1234@users.noreply.github.com>
2020-10-15 22:27:40 -0700

    Add files via upload

--- Basics/Assignment2.4/Bar.java
+++ Basics/Assignment2.4/Bar.java
@@ -12,8 +12,6 @@ class Bar {
         System.out.print("Please indicate the number of characters to be displayed. ");
         x = sc.nextInt();
         
-        System.out.println("----------------------------------------------------------------");
-        
         for (int n = 1; n <= x; n++) {
             System.out.print(input);
         }
--- Basics/Assignment2.4/Triangle.java
+++ Basics/Assignment2.4/Triangle.java
@@ -11,12 +11,10 @@ class Triangle {
 
         System.out.print("Please indicate the height of the triangle. ");
         x = sc.nextInt();
-
-        System.out.println("----------------------------------------------------------------");
         
         for (int n = 1; n <= x; n++) {
             for (int m = 1; m <= n; m++) {
-                System.out.print(input);
+                System.out.print(input + " ");
             }
             System.out.println("");
         }
