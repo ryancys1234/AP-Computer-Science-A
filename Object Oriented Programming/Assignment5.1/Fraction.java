f84b98115b454bf67b18be6b5243eb121c0f520b
ryancys1234 <55104016+ryancys1234@users.noreply.github.com>
2021-02-02 11:30:16 -0800

    Add files via upload

--- Object Oriented Programming/Assignment5.1/Fraction.java
+++ Object Oriented Programming/Assignment5.1/Fraction.java
@@ -1,6 +1,8 @@
 class Fraction {
     private int num, denom;
     
+    // Constructors
+    
     public Fraction() {
         num = 1;
         denom = 2;
@@ -10,7 +12,7 @@ class Fraction {
         this.num = n;
         
         if (d == 0) {
-            System.out.println("Error: the denominator is 0");
+            System.out.println("Error: the denominator is 0.");
             this.denom = 1;
         } else this.denom = d;
     }
@@ -19,21 +21,61 @@ class Fraction {
         this.num = Integer.parseInt(f.substring(0, f.indexOf("/")));
         
         if (Integer.parseInt(f.substring(f.indexOf("/"), f.length() - 1)) == 0) {
-            System.out.println("Error: the denominator is 0");
+            System.out.println("Error: the denominator is 0.");
             this.denom = 1;
         } else this.denom = Integer.parseInt(f.substring(f.indexOf("/"), f.length() - 1));
     }
     
     public Fraction(Fraction f) {
         this.num = f.num;
-        this.denom = f.denom;
+        if (f.denom == 0) {
+            System.out.println("Error: the denominator is 0.");
+            this.denom = 1;
+        } else this.denom = f.denom;
     }
+    
+    // Accessor Methods
     
     public int getNum() {
         return this.num;
     }
     
     public int getDenom() {
+        if (this.denom == 0) {
+            System.out.println("Error: the denominator is 0.");
+            this.denom = 1;
+        }
+        
         return this.denom;
+    }
+    
+    public String toString() {
+        if (this.denom == 0) {
+            System.out.println("Error: the denominator is 0.");
+            return this.num + " / " + 1;
+        } else return this.num + " / " + this.denom;
+    }
+    
+    // Mutator Methods
+    
+    public void setNum(int num) {
+        this.num = num;
+    }
+    
+    public void setDenom(int denom) {
+        this.denom = denom;
+    }
+    
+    // Static Methods
+    
+    // Helper Method
+    
+    public int gcf(int a, int b) {
+        while (a != b) {
+            int diff = Math.abs(b - a);
+            b = diff;
+        }
+        
+        return a;
     }
 }
--- Object Oriented Programming/Assignment5.1/FractionDriver.java
+++ Object Oriented Programming/Assignment5.1/FractionDriver.java
@@ -1,5 +1,9 @@
 class FractionDriver {
     public static void main(String[] args) {
+        Fraction myFraction = new Fraction();
+        Fraction myFraction1 = new Fraction(1, 2);
+        Fraction myFraction2 = new Fraction(1, 0);
         
+        System.out.println(myFraction.gcf(252, 105));
     }
 }
